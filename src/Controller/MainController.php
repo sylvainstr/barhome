80a0bd625aebd4ee79f3dc4a97f6b1c1157a6cd9
sylvainstr <sylvain.sainteromane@gmail.com>
2022-08-19 12:36:23 +0200

    add contact page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use App\Service\SendMailService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,8 +18,47 @@ class MainController extends AbstractController
     }
     
     #[Route('/contact', name: 'contact')]
-    public function contact(): Response
+    public function contact(Request $request, SendMailService $mail): Response
     {
-        return $this->render('main/contact.html.twig');
+        $form = $this->createForm(ContactType::class);
+
+        $contact = $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $context = [
+                'name' => $contact->get('name')->getData(),
+                'firstname' => $contact->get('firstname')->getData(),
+                'mail' => $contact->get('email')->getData(),
+                'phone' => $contact->get('phone')->getData(),
+                'subject' => $contact->get('subject')->getData(),
+                'message' => $contact->get('message')->getData(),
+            ];
+            $mail->send(
+                $contact->get('email')->getData(),
+                'sylvain.sainteromane@gmail.com',
+                'Bar Home - Contact',
+                'contact',
+                $context
+            );
+            
+            // accusé réception
+            $mail->send(
+                'sylvain.sainteromane@gmail.com',
+                $contact->get('email')->getData(),
+                "Bar Home - Nous avons bien reçu votre message",
+                'message_confirmation',
+                $context
+            );
+            
+            $this->addFlash('success', 'Vore message a bien été envoyé');
+            return $this->redirectToRoute('homepage');
+         
+        }
+
+        return $this->render('main/contact.html.twig', [
+            'contact_form' => $form->createView()
+        ]);
+        
     }
+
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',TextType::class, [
+                "label" => "Votre nom* :",
+                'attr' => ['placeholder' => "Exemple : Dupont"],
+                'required' => true
+            ])
+            ->add('firstname',TextType::class, [
+                "label" => "Votre prénom* :",
+                'attr' => ['placeholder' => "Exemple : Marie"],
+                'required' => true
+            ])
+            ->add('email',EmailType::class, [
+                "label" => "Votre adresse e-mail* :",
+                'attr' => ['placeholder' => "Exemple : dupont.marie@mail.com"],
+                'required' => true
+            ])
+            ->add('phone',TextType::class, [
+                "label" => "Numéro de téléphone* :",
+                'attr' => ['placeholder' => "Exemple : 0600000000"],
+                'required' => true
+                ])
+            ->add('subject',TextType::class, [
+                "label" => "Votre sujet* :",
+                'attr' => ['placeholder' => "Exemple : Demande de devis"],
+                'required' => true
+                ])
+            ->add('message', TextareaType::class, [
+                "label" => "Votre message* :",
+                'attr' => ['placeholder' => "Tapez votre message ici..."],
+                'required' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
