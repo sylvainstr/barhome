66a481fc23a3e4d4e788647d22d50ed4fdbc5513
sylvainstr <sylvain.sainteromane@gmail.com>
2022-09-05 10:55:06 +0200

    add regex on drink form

--- src/Form/DrinkType.php
+++ src/Form/DrinkType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class DrinkType extends AbstractType
 {
@@ -17,16 +18,40 @@ class DrinkType extends AbstractType
         ->add('category',TextType::class, [
             "label" => "Catégorie de la boisson*",
             'attr' => ['placeholder' => "Veuillez renseigner la boisson"],
+            'constraints' => [ 
+                new Regex(
+                    [
+                        'pattern' => '/^[\p{Ll}À-ÖØ-öø-ÿ-]+$/',
+                        'message' => 'Votre texte doit être en minuscule'
+                    ]
+                ),
+            ],
             'required' => true
         ])        
         ->add('name',TextType::class, [
             "label" => "Nom de la boisson*",
             'attr' => ['placeholder' => "Veuillez renseigner le nom"],
+            'constraints' => [ 
+                new Regex(
+                    [
+                        'pattern' => '/^[\p{Ll}À-ÖØ-öø-ÿ-]+$/',
+                        'message' => 'Votre texte doit être en minuscule'
+                    ]
+                ),
+            ],
             'required' => true
         ])
         ->add('year',IntegerType::class, [
             "label" => "Année",
             'attr' => ['placeholder' => "Veuillez renseigner l'année"],
+            'constraints' => [ 
+                new Regex(
+                    [
+                        'pattern' => '/^\d{4}$/',
+                        'message' => 'Votre date doit contenir 4 chiffres'
+                    ]
+                ),
+            ],
             'required' => false
         ])
         ;
