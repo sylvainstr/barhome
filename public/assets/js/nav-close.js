5b6f924d1bfba7d8823f47bdc08e3d507d76f2e5
sylvainstr <sylvain.sainteromane@gmail.com>
2022-09-05 16:09:39 +0200

    add burger menu

---
+++ public/assets/js/nav-close.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const navClose = () => {
+
+  
+  const navbarOpen = document.querySelector('.nav-mob.show-nav');
+  const navItems = document.querySelectorAll(".nav-item");
+
+ 
+
+    const navbar = document.querySelector('.nav-mob');
+    const burger = document.querySelector('.burger');
+    const mainNav = document.querySelector(".main-nav");
+
+    navItems.forEach(item => {
+
+      item.addEventListener('click', (e) => {
+
+        var width = window.innerWidth;
+
+        if (width < 800) {
+          mainNav.classList.toggle('show-nav');
+          navbar.classList.toggle('show-nav');
+        }
+      })
+    })
+  
+}
+
+navClose();
+
+});
